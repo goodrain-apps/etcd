7d9f9d01fbc0ff12f49ce134b79023705c1f6a7d
zhouyq <zhouyq@goodrain.com>
2018-01-22 14:35:34 +0800

    add node name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+NODE_NAME=${SERVICE_NAME:-etcd01}
+
 [[ $DEBUG ]] && set -x
 
 
 sleep ${PAUSE:-0}
 
-exec /opt/goodrain/etcd/etcd --data-dir /data/
+
+exec /opt/goodrain/etcd/etcd --name=${NODE_NAME} --data-dir /data/

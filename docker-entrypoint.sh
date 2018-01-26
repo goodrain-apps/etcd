095341ffd9627794c2d6e8ce0b4d6a2c7e384abb
zhouyq <zyq916@gmail.com>
2018-01-26 10:37:15 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,4 @@ NODE_NAME=${SERVICE_NAME:-etcd01}
 sleep ${PAUSE:-0}
 
 
-exec /opt/goodrain/etcd/etcd --name=${NODE_NAME} --data-dir /data/
+exec /opt/goodrain/etcd/etcd --name=${NODE_NAME} --data-dir /data/  --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001

34e557c946cf0908d4d26b11e5c90675746f87cd
zhouyq <zyq916@gmail.com>
2018-01-26 10:44:16 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,15 @@ NODE_NAME=${SERVICE_NAME:-etcd01}
 sleep ${PAUSE:-0}
 
 
-exec /opt/goodrain/etcd/etcd --name=${NODE_NAME} --data-dir /data/  --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001
+exec /opt/goodrain/etcd/etcd \
+     --name=${NODE_NAME} \
+     --data-dir /data/  \
+     --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
+     --advertise-client-urls http://127.0.0.1:2379,http://127.0.0.1:4001 \
+     --initial-advertise-peer-urls http://127.0.0.1:2380 \
+     --listen-peer-urls http://0.0.0.0:2380 \
+     --initial-cluster-token etcd-cluster-1 \
+     --initial-cluster etcd0=http://127.0.0.1:2380 \
+     --initial-cluster-state new
+      
+

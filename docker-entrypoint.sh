da47c0a2559ea334eb38cbaa2552dc21f47a5c72
zhoujunhao <18853925545@163.com>
2018-06-28 14:40:49 +0800

    [FIX] fix bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-NODE_NAME=${SERVICE_NAME:-etcd01}
-
-
 [[ $DEBUG ]] && set -x
 
-cd $TIDB_HOME
 SELF_IP=$(ping -c 1 `hostname`|head -n 1 | awk -F '[()]' '{print $2}')
 SELF_ID=${HOSTNAME##*-}
 HOST_NAME=`hostname -f`
 
-peers=`net lookupsrv -h $SERVICE_NAME --format http://%s:4001`
+peers=`net lookupsrv -h $SERVICE_NAME --format http://%s:2380`
 
 if [[ x$peers == x ]]; then
-  start_cmd="--initial-cluster pd$SELF_ID=http://$HOST_NAME:4001"
+  start_cmd="--initial-cluster pd$SELF_ID=http://$HOST_NAME:2380"
 else
   start_cmd="--join $peers"
   sleep 3
@@ -25,10 +21,10 @@ fi
 exec /opt/goodrain/etcd/etcd \
      --name="pd$SELF_ID" \
      --data-dir /data/  \
-     --listen-client-urls http://$SELF_IP:2379,http://127.0.0.1:2379 \
+     --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
      --advertise-client-urls http://$SELF_IP:2379,http://$SELF_IP:4001 \
-     --initial-advertise-peer-urls http://$HOST_NAME:4001 \
-     --listen-peer-urls http://$HOST_NAME:4001 \
+     --initial-advertise-peer-urls http://$HOST_NAME:2380 \
+     --listen-peer-urls http://0.0.0.0:2380 \
      --initial-cluster-token etcd-cluster-1 \
      --initial-cluster-state new \
      $start_cmd

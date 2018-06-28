61b396717800e988aa8fb434716723a8d25be221
zjhgoodrain <38780264+zjhgoodrain@users.noreply.github.com>
2018-06-28 19:17:37 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,20 +7,33 @@ SELF_ID=${HOSTNAME##*-}
 HOST_NAME=`hostname -f`
 
 chmod +x /usr/bin/net
-peers=`net lookupsrv -h $SERVICE_NAME --format http://%s:2380`
+peers=`net lookupsrv -h $SERVICE_NAME --wrap`
 
 if [[ x$peers == x ]]; then
-  start_cmd="--initial-cluster pd$SELF_ID=http://$HOST_NAME:2380"
+    start_cmd="--initial-cluster node$SELF_ID=http://$HOST_NAME:2380"
 else
-  start_cmd="--initial-cluster $peers"
-  sleep 3
+    peers_http=""
+    for peer in $peers; do
+    id=${peer%%.*}
+    id=${id##*-}
+    peer="node${id}=$peer"
+
+if [[ x$peers_http == "" ]]; then
+    peers_http="$peer"
+else
+    peers_http="${peers_http},$peer"
 fi
 
-# sleep ${PAUSE:-0}
+done
+
+    start_cmd=$peers_http
+    sleep 3
+fi
+
 
 
 exec /opt/goodrain/etcd/etcd \
-     --name="pd$SELF_ID" \
+     --name="node$SELF_ID" \
      --data-dir /data/  \
      --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
      --advertise-client-urls http://$SELF_IP:2379,http://$SELF_IP:4001 \
@@ -29,5 +42,3 @@ exec /opt/goodrain/etcd/etcd \
      --initial-cluster-token etcd-cluster-1 \
      --initial-cluster-state new \
      $start_cmd
-      
-

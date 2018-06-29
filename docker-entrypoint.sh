9fc364dd81974a9307fd405009db6c0e588b2e7e
zjhgoodrain <38780264+zjhgoodrain@users.noreply.github.com>
2018-06-29 14:33:05 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -46,4 +46,5 @@ exec /opt/goodrain/etcd/etcd \
      --advertise-client-urls http://$SELF_IP:2379 \
      --initial-advertise-peer-urls http://$peerx:2380 \
      --listen-peer-urls http://$HOST_NAME:2380 \
-     --initial-cluster-token etcd-cluster-1  $start_state $start_cmd
+     --initial-cluster-token etcd-cluster-1 $start_state \
+     $start_cmd

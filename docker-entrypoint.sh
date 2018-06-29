59ad9c70990d04725ffc3edc0582492e313a36df
zjhgoodrain <38780264+zjhgoodrain@users.noreply.github.com>
2018-06-29 16:53:20 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,7 @@ peers=`net lookupsrv -h $SERVICE_NAME --wrap`
 
 if [[ x$peers == x ]]; then
     export ETCD_INITIAL_CLUSTER_STATE=new
-    export ETCD_INITIAL_CLUSTER="node$SELF_ID=http://$HOST_NAME:2380"
+    # export ETCD_INITIAL_CLUSTER="node$SELF_ID=http://$HOST_NAME:2380"
     # start_cmd="--initial-cluster node$SELF_ID=http://$HOST_NAME:2380"
     # start_state="--initial-cluster-state new"
 else
